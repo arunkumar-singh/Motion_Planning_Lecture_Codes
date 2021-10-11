3a602b2e9ad4320cb0ca336819907585090dab49
arun <aks1812@gmail.com>
2021-10-12 01:29:29 +0300

    updating

---
+++ manipulator_planning/robot_toolox_check_1.py
@@ -0,0 +1,38 @@
+
+
+import numpy as np
+import roboticstoolbox as rtb
+robot = rtb.models.DH.Planar3()
+
+from spatialmath import SE3
+
+# T = SE3(0.7, 0.2, 0.1) * SE3.OA([0, 1, 0], [0, 0, -1])
+# sol = robot.ikine_LM(T)         # solve IK
+# print(sol)
+
+# q_pickup = sol.q
+# print(robot.fkine(q_pickup))    # FK shows that desired end-effector pose was achieved
+
+
+# q_min = np.array([-165, -100, -165, -165, -165, -1.0, -165  ])*np.pi/180
+# q_max = np.array([ 165,   101,  165,  1.0,    165, 214, 165  ])*np.pi/180
+
+# q_pickup = np.random.uniform(q_min, q_max, (7, ))
+
+q_min = -(np.pi/4)*np.ones(3)
+q_max = (np.pi/3)*np.ones(3)
+
+
+q_pickup = np.random.uniform(q_min, q_max, (3, ))
+
+print(robot.qz)
+
+
+
+
+qt = rtb.jtraj(robot.qz, q_pickup, 50)
+robot.plot(qt.q, movie='planar.gif')
+
+
+
+print(robot)
